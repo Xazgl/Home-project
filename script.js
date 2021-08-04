5588b621362833d973f899cf881c308e46a2ce7e
Uriy Jukov <ldomofon-sericel@rambler.ru>
2021-08-04 22:15:22 +0300

    04.08.21

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let btn = document.getElementById('btn');
+
+let inputdiv = document.getElementById('inputdiv');
+let input = document.createElement('input');
+let button = document.createElement('button_input');
+
+inputdiv.append(input)
+inputdiv.append(button_input)
+
+
+button.textContent = 'отправить';
+
+
+
+
+
+/*let btn = document.getElementById('btn');
 let startCursorX;
 let startCursorY;
 let startX;
@@ -9,8 +24,9 @@ let startY;
 	  startX = btn.style.marginLeft.replace('px','')*1; // Нам нужны только цыфры без PX
 	  startY = btn.style.marginTop.replace('px','')*1;
 	});
+	
 	btn.addEventListener('dragend',function() {
         btn.style.position = 'absolute';//CSS теперь элемент "Блуждающий" 
         btn.style.marginLeft = startX + event.pageX-startCursorX; //позиция элемента + позиция курсора - позиция курсоа в начале перетаскивания
         btn.style.marginTop = startY + event.pageY-startCursorY; // Так же как и в предыдущем случае, только по другой оси
-    });
+    }); */
